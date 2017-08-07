b3aa7df70b50ee25499bc5c49c4e439f032617b2
ikramkhan3133 <khanikram3133@yahoo.com>
2017-08-07 14:34:24 -0700

    erasfd
    first project

---
+++ PatientService1.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace FirstProjectWcfService6
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class PatientService1 : IPatientService1
+    {
+
+        public List<patient> Getallpatient()
+        {
+            using (var db = new WcfServiceDataBaseEntities())
+            {
+                return db.patients.ToList();
+            }
+
+        }
+
+
+        public patient GetPatient(string id)
+        {
+            Int32 Id = Convert.ToInt32(id);
+            using(var db = new WcfServiceDataBaseEntities())
+            {
+                return db.patients.FirstOrDefault(p => p.Id == p.Id);
+            }
+
+        }
+    }
+}
